95ab4a2c6efcaac0cfc0d597f1f05959264c0367
maxbroxgames <124490874+maxbroxgames@users.noreply.github.com>
2023-03-12 09:37:36 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+function solution() {
+    let a = document.getElementById('a').value;
+    let b = document.getElementById('b').value;
+    document.getElementById('sum').innerText = +a + +b;
+    }
+ function solution1() {
+        let a = document.getElementById('a').value;
+        let b = document.getElementById('b').value;
+        document.getElementById('sum').innerText = +a - +b;
+        }
+        function solution2() {
+            let a = document.getElementById('a').value;
+            let b = document.getElementById('b').value;
+            document.getElementById('sum').innerText = +a * +b;
+            }
+  function solution3() {
+                let a = document.getElementById('a').value;
+                let b = document.getElementById('b').value;
+                document.getElementById('sum').innerText = +a / +b;
+ }
